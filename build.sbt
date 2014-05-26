d11ccf7d134c4de95a9db7f449d86912ac9817a4
Fabrice Claes <Fabrice.Claes@gmail.com>
2014-05-26 19:59:19 +0200

    Correction in test, datasource included

--- build.sbt
+++ build.sbt
@@ -18,6 +18,10 @@ libraryDependencies += "org.mockito" % "mockito-core" % "1.9.5" % "test"
 
 libraryDependencies += "com.h2database" % "h2" % "1.3.171" % "test"
 
+libraryDependencies += "org.apache.commons" % "commons-dbcp2" % "2.0.1"
+
+libraryDependencies += "org.slf4j" % "jcl-over-slf4j" % "1.7.5"
+
 scalacOptions ++= Seq("-feature", "-deprecation")
 
 //(testOptions in Test) += Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/report")
--- src/test/scala/org/hittepit/smapapi/mapper/TestMapper.scala
+++ src/test/scala/org/hittepit/smapapi/mapper/TestMapper.scala
@@ -5,54 +5,87 @@ import java.sql.ResultSet
 import org.scalatest.GivenWhenThen
 import org.scalatest.MustMatchers
 import java.sql.DriverManager
+import java.util.logging.Logger
+import org.slf4j.LoggerFactory
+import org.apache.commons.dbcp2.BasicDataSource
 
-case class Book(id:Option[Int],isbn:String, title:String, author:Option[String])
+case class Book(id: Option[Int], isbn: String, title: String, author: Option[String])
 
-class BookMapper extends Mapper[Book,Int]{
+class BookMapper extends Mapper[Book, Int] {
   override val pk = Some(id)
-  val tableName="BOOK"
-  def id = generatedPrimaryKey("id",Nullable(Integer),_.id)
-  def isbn = column("isbn",Varchar, _.isbn)
-  def title = column("title",NotNullable(Varchar),_.title)
-  def author = column("author", Nullable(Varchar),_.author)
-  def map(implicit rs:ResultSet) = Book(id,isbn,title,author)
-  val insertable = List(isbn,title,author)
-  val updatable =  List(isbn,title,author)
+  val tableName = "BOOK"
+  def id = generatedPrimaryKey("id", Nullable(Integer), _.id)
+  def isbn = column("isbn", Varchar, _.isbn)
+  def title = column("title", NotNullable(Varchar), _.title)
+  def author = column("author", Nullable(Varchar), _.author)
+  def map(implicit rs: ResultSet) = Book(id, isbn, title, author)
+  val insertable = List(isbn, title, author)
+  val updatable = List(isbn, title, author)
 }
 
-class TestMapper extends WordSpec with MustMatchers{
-  	val mapper = new BookMapper
-  	Class.forName("org.h2.Driver")
-  	
-  	def getConnection = {
-  		val c = DriverManager.getConnection("jdbc:h2:mem:test1")
-	  	var st = c.prepareStatement("create table BOOK (id integer,isbn varchar(10),title varchar(20),author varchar(20))")
-	  	st.execute()
-	  	st.close
-	  	st = c.prepareStatement("insert into book (id,isbn,title,author) values (1,'12312','Test','toto')")
-	  	st.execute()
-	  	st.close
-	  	c
-  	}
+class DataSource extends BasicDataSource {
+  val logger = LoggerFactory.getLogger("datasource")
+  this.setDriverClassName("org.h2.Driver")
+  this.setUsername("h2")
+  this.setUrl("jdbc:h2:mem:test;MVCC=TRUE")
+}
 
-	"An implemented mapper" when {
-	  "asked for insert sql String" must {
-	    "return a String ready for PreparedStatement" in {
-	    	mapper.insertSqlString must be("insert into BOOK (isbn,title,author) values (?,?,?)")
-	    }
-	  }
-	  
-	  "find" must {
-	    "return Some(b)" in {
-	      val b = mapper.find(1)(getConnection)
-	      b match {
-	        case Some(book) => book.id must be(Some(1))
-	        					book.title must be("Test")
-	        					book.isbn must be("12312")
-	        					book.author must be(Some("toto"))
-	        case None => fail
-	      }
-	    }
-	  }
-	}
+class TestMapper extends WordSpec with MustMatchers {
+  val mapper = new BookMapper
+  val datasource = new DataSource
+
+  val c = datasource.getConnection
+  var st = c.createStatement
+  st.addBatch("create table BOOK (id integer,isbn varchar(10),title varchar(20),author varchar(20));")
+  st.addBatch("insert into book (id,isbn,title,author) values (1,'12312','Test','toto');")
+  st.addBatch("insert into book (id,isbn,title,author) values (2,'12313','Test2',null);")
+  st.executeBatch()
+  st.close
+  c.close
+
+  "An implemented mapper" when {
+    "asked for insert sql String" must {
+      "return a String ready for PreparedStatement" in {
+        mapper.insertSqlString must be("insert into BOOK (isbn,title,author) values (?,?,?)")
+      }
+    }
+  }
+
+  "The find method of a mapper" when {
+    "called with an existing id with not null author" must {
+      "return Some(b), b with Some(author)" in {
+        val b = mapper.find(1)(datasource.getConnection)
+        b match {
+          case Some(book) =>
+            book.id must be(Some(1))
+            book.title must be("Test")
+            book.isbn must be("12312")
+            book.author must be(Some("toto"))
+          case None => fail
+        }
+      }
+    }
+    "called with an existing id with a null author" must {
+      "return Some(b), author beigin None" in {
+        val b = mapper.find(2)(datasource.getConnection)
+        b match {
+          case Some(book) =>
+            book.id must be(Some(2))
+            book.title must be("Test2")
+            book.isbn must be("12313")
+            book.author must be(None)
+          case None => fail
+        }
+      }
+    }
+    "called with a non existing id" must {
+      "return None" in {
+        val b = mapper.find(40)(datasource.getConnection)
+        b match {
+          case Some(book) => fail
+          case None =>
+        }
+      }
+    }
+  }
 }
