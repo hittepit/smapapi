5f0f991a77ca2b5414f661cc68b7af294fc23c2d
hittepit <fabrice.claes@gmail.com>
2014-06-03 13:24:32 +0200

    Tests on JdbcTestTransaction

--- src/test/scala/org/hittepit/smapapi/test/TestJdbcTestTransaction.scala
+++ src/test/scala/org/hittepit/smapapi/test/TestJdbcTestTransaction.scala
@@ -1,23 +1,50 @@
 package org.hittepit.smapapi.test
 
 import org.scalatest.WordSpec
+import org.hittepit.smapapi.transaction.TransactionManager
+import java.sql.Connection
+import javax.sql.DataSource
+import org.slf4j.LoggerFactory
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+import org.scalatest.MustMatchers
 
-class TestJdbcTestTransaction extends WordSpec {
-  "inTransactionTest" when {
-    "invoked on a function that succeed" must {
-      "mark the transaction for rollback" in {
-        pending
+class TestJdbcTestTransaction extends WordSpec with MustMatchers with MockitoSugar {
+  trait TestTransactionalEnvironment{
+    val connection = mock[Connection]
+    val ds = mock[DataSource]
+	when(ds.getConnection()).thenReturn(connection)
+    val jdbcTransaction = new JdbcTestTransaction{
+      val logger = LoggerFactory.getLogger(classOf[JdbcTestTransaction])
+      val transactionManager=new TransactionManager{
+    	  val logger = LoggerFactory.getLogger(classOf[TransactionManager])
+    	  val dataSource = ds;
       }
-      "return the result of the function" in {
-        pending
+    }
+  }
+  
+  "withRollback" when {
+    "invoked on a function that succeed" must {
+      "mark the transaction for rollback" in new TestTransactionalEnvironment{
+        jdbcTransaction.withRollback{con => 7}
+        verify(connection, times(1)).rollback()
+      }
+      "return the result of the function" in new TestTransactionalEnvironment{
+        val r = jdbcTransaction.withRollback{con => 7}
+        r must be(7)
       }
     }
     "invoked on a function that throws an exception" must {
-      "mark the transaction for rollback" in {
-        pending
+      "mark the transaction for rollback" in new TestTransactionalEnvironment{
+        try{
+        	jdbcTransaction.withRollback{con => throw new Exception}
+        }catch{
+          case _:Throwable =>
+        }
+        verify(connection, times(1)).rollback()
       }
-      "rethrow the exception" in {
-        pending
+      "rethrow the exception" in new TestTransactionalEnvironment{
+        an[Exception] must be thrownBy(jdbcTransaction.withRollback{con => throw new Exception})
       }
     }
   }
