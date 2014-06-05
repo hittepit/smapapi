a3af77712a36224f46aedae391d590aaa8435df2
hittepit <fabrice.claes@gmail.com>
2014-06-05 14:42:53 +0200

    Implemnting session methods

--- src/main/scala/org/hittepit/smapapi/core/Session.scala
+++ src/main/scala/org/hittepit/smapapi/core/Session.scala
@@ -7,27 +7,40 @@ import java.sql.ResultSetMetaData
 
 case class Param[T](value:T,sqlType:SqlType[T])
 
+class QueryResult(val rs: ResultSet){
+  def map[T](mapper: Row => T):List[T] = {
+    def innerMap(acc:List[T]):List[T] = if(rs.next){
+      innerMap(acc:::List(mapper(new Row(rs))))
+    } else {
+      acc
+    }
+    
+    innerMap(Nil)
+  }
+}
 
-class Row(resultSet:ResultSet,rsmd:ResultSetMetaData){
+class Row(resultSet:ResultSet){
   def getColumnValue[T](index:Int,sqlType:SqlType[T]) = sqlType.getColumnValue(resultSet,Right(index))
   def getColumnValue[T](columnName:String,sqlType:SqlType[T]) = sqlType.getColumnValue(resultSet,Left(columnName))
 
 }
 
 class Session(val connection:Connection) {
-	def streamRs(rsmd:ResultSetMetaData,rs:ResultSet):Stream[Row] = if(rs.next()) new Row(rs,rsmd) #:: streamRs(rsmd,rs) else Stream.Empty
-	
-	def select(sql:String,params:List[Param[_]]):Stream[Row] = {
+	def select(sql:String,params:List[Param[_]]):QueryResult = {
 	  val ps = connection.prepareStatement(sql)
 	  
 	  params.zipWithIndex.foreach{_ match{
 	    case (param:Param[_],index) => param.sqlType.setColumnValue(index+1,param.value,ps)
 	  }}
 	  
-	  streamRs(ps.getMetaData(),ps.executeQuery)
+	  new QueryResult(ps.executeQuery)
 	}
 	
-	def select[T](sql:String,params:List[Param[_]],mapper:Row => T):Seq[T] = {
-	  select(sql,params) map (mapper)
+	def select[T](sql:String,params:List[Param[_]],mapper:Row => T):Seq[T] = select(sql,params) map (mapper)
+	
+	def unique[T](sql:String,params:List[Param[_]],mapper:Row => T):Option[T] = select(sql,params) map (mapper) match {
+	  case Nil => None
+	  case List(t) => Some(t)
+	  case _ => throw new Exception("More than one result") //TODO exception 
 	}
 }
---
+++ src/test/scala/org/hittepit/smapapi/core/TestSelectionsInSession.scala
@@ -0,0 +1,144 @@
+package org.hittepit.smapapi.core
+
+import org.apache.commons.dbcp.BasicDataSource
+import java.sql.Connection
+import org.scalatest.WordSpec
+import org.scalatest.BeforeAndAfter
+import org.hittepit.smapapi.mapper.NotNullableDouble
+import org.hittepit.smapapi.mapper.NotNullableVarchar
+import org.hittepit.smapapi.mapper.NullableVarchar
+import org.scalatest.MustMatchers
+import org.hittepit.smapapi.mapper.NotNullableInteger
+
+class TestSelectionsInSession extends WordSpec with BeforeAndAfter with MustMatchers{
+  class DataSource extends BasicDataSource {
+    this.setDriverClassName("org.h2.Driver")
+    this.setUsername("h2")
+    this.defaultAutoCommit = false
+    this.defaultTransactionIsolation = Connection.TRANSACTION_READ_COMMITTED
+    this.setUrl("jdbc:h2:mem:test;MVCC=TRUE")
+  }
+
+  val ds = new DataSource
+
+  before {
+    val connection = ds.getConnection()
+    var st = connection.createStatement
+    st.addBatch("create table BOOK (id integer auto_increment,isbn varchar(10),title varchar(20),author varchar(20), price double, PRIMARY KEY(id));")
+    st.addBatch("insert into book (id,isbn,title,author,price) values (1,'12312','Test','toto',10.40);")
+    st.addBatch("insert into book (id,isbn,title,author,price) values (2,'12313','Test2',null,5.60);")
+    st.addBatch("insert into book (id,isbn,title,author,price) values (3,'12314','Test3','toto',2.25);")
+    st.executeBatch()
+    connection.commit()
+    connection.close
+  }
+
+  after {
+    val connection = ds.getConnection()
+    var st = connection.createStatement
+    st.addBatch("delete from book");
+    st.addBatch("drop table BOOK;")
+    st.executeBatch()
+    connection.commit()
+    connection.close
+  }
+
+  "The select method" when {
+    "called without mapper" must {
+      "generate a QueryResult that can be mapped to generate ojects" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        
+        val rows = session.select("select isbn,author from book where price > ?",List(Param(5.0,NotNullableDouble)))
+        
+        val tuples = rows map {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        connection.close
+        
+        tuples.size must be(2)
+        tuples must contain(("12312",Some("toto")))
+        tuples must contain(("12313",None))
+      }
+    }
+    
+    "called with a mapper and a query that has results" must {
+      "generate a list of objects" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        val mapper = {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        val tuples = session.select("select isbn,author from book where price > ?",List(Param(5.0,NotNullableDouble)),mapper)
+        
+        connection.close
+        
+        tuples.size must be(2)
+        tuples must contain(("12312",Some("toto")))
+        tuples must contain(("12313",None))
+      }
+    }
+    
+    "called with a mapper and a query that results in nothing" must {
+      "generate an empty list" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        val mapper = {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        val tuples = session.select("select isbn,author from book where price < ?",List(Param(0.0,NotNullableDouble)),mapper)
+        
+        connection.close
+        
+        tuples must be(Nil)
+      }
+    }
+  }
+  
+  "The unique method" when {
+    "called with a request that returns one row" must{
+      "return Some(object)" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        val mapper = {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        val tuple = session.unique("select isbn,author from book where id = ?",List(Param(1,NotNullableInteger)),mapper)
+        
+        tuple must be(Some(("12312",Some("toto"))))
+        connection.close
+      }
+    }
+    "called with a request that returns no row" must{
+      "return None" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        val mapper = {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        val tuple = session.unique("select isbn,author from book where id = ?",List(Param(1000,NotNullableInteger)),mapper)
+        
+        tuple must be(None)
+        connection.close
+      }
+    }
+    "called with a request that returns more than one row" must{
+      "throw an exception" in {
+        val connection = ds.getConnection()
+        val session = new Session(connection)
+        val mapper = {row:Row =>
+          (row.getColumnValue("isbn", NotNullableVarchar),row.getColumnValue("author", NullableVarchar))
+        }
+        
+        an [Exception] must be thrownBy (session.unique("select isbn,author from book where id > ?",List(Param(1,NotNullableInteger)),mapper))
+        
+        connection.close
+      }
+    }
+  }
+}
