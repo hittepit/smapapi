137a85a391025c8de57ba7ec7ab69fe3ae646e73
hittepit <fabrice.claes@gmail.com>
2014-06-25 09:45:17 +0200

    ScalaDoc on exceptions

--- src/main/scala/org/hittepit/smapapi/core/exception/Exceptions.scala
+++ src/main/scala/org/hittepit/smapapi/core/exception/Exceptions.scala
@@ -1,6 +1,15 @@
 package org.hittepit.smapapi.core.exception
 
+/**
+ * Lancée lorsqu'une valeur non nulle est attendue, mais que la valeur (dans la colonne du ResultSet) est nulle.
+ * 
+ * Elle survient dans les [[org.hittepit.smapapi.core.PropertyType PropertyType]] non optionnels. la solution consiste à utiliser un type optionnel ou à créer
+ * son propre [[org.hittepit.smapapi.core.PropertyType PropertyType]].
+ */
 class NullValueException(msg:String) extends RuntimeException(msg)
 
+/**
+ * Lancée lorsqu'un résultat unique est attendu, mais que plusieurs résultats sont renvoyés.
+ */
 class NotUniqueResultException(msg:String) extends RuntimeException(msg)
 
